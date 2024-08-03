dddf55b95ee36a37111b1e0594b3100ef38fa65b
Joe <jihwan3331@gmail.com>
2024-08-03 10:58:08 +0900

    first commit

---
+++ build/mission_control/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/joe/ysdrone_2024/install/px4_msgs/share/px4_msgs/package.sh"
---
+++ build/px4_msgs/px4_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/joe/ysdrone_2024/build/px4_msgs/px4_msgs__py
+# Build directory: /home/joe/ysdrone_2024/build/px4_msgs/px4_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
