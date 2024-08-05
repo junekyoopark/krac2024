b824b87642f8a7a67bda57ee0361be9da88a418b
john <junekyoopark@gmail.com>
2024-08-06 04:00:51 +0900

    update static tf use_sim_time parameters - not sure if theyre needed. also updated map to odom tf broadcaster to change data type to float

--- src/dontscrewthepooch/dontscrewthepooch/tf2_map_to_odom.py
+++ src/dontscrewthepooch/dontscrewthepooch/tf2_map_to_odom.py
@@ -1,18 +1,26 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
-from px4_msgs import VehicleOdometry
+from px4_msgs.msg import VehicleOdometry
 
 class OdometryTFBroadcaster(Node):
     def __init__(self):
         super().__init__('odometry_tf_broadcaster')
         # Subscribe to the vehicle odometry topic
+        # Configure QoS profile for publishing and subscribing
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
         self.subscription = self.create_subscription(
             VehicleOdometry,
             'fmu/out/vehicle_odometry',
             self.handle_vehicle_odometry,
-            10)
+            qos_profile)
         self.br = tf2_ros.TransformBroadcaster(self)
         self.get_logger().info("Vehicle Odometry TF broadcaster has been started")
 
@@ -25,14 +33,14 @@ class OdometryTFBroadcaster(Node):
         t.child_frame_id = 'odom'  # Assume odom as the child frame
         # Ensure to handle NaNs and set default values if necessary
         if not any(nan in msg.position for nan in (float('nan'),)):
-            t.transform.translation.x = msg.position[0]
-            t.transform.translation.y = msg.position[1]
-            t.transform.translation.z = msg.position[2]
+            t.transform.translation.x = float(msg.position[0])
+            t.transform.translation.y = float(msg.position[1])
+            t.transform.translation.z = float(msg.position[2])
         if not any(nan in msg.q for nan in (float('nan'),)):
-            t.transform.rotation.x = msg.q[0]
-            t.transform.rotation.y = msg.q[1]
-            t.transform.rotation.z = msg.q[2]
-            t.transform.rotation.w = msg.q[3]
+            t.transform.rotation.x = float(msg.q[0])
+            t.transform.rotation.y = float(msg.q[1])
+            t.transform.rotation.z = float(msg.q[2])
+            t.transform.rotation.w = float(msg.q[3])
 
         # Broadcast the transform
         self.br.sendTransform(t)
--- src/dontscrewthepooch/launch/dontscrewthepooch.launch.py
+++ src/dontscrewthepooch/launch/dontscrewthepooch.launch.py
@@ -1,23 +1,36 @@
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
-
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
     static_tf2_odom_to_base = Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            arguments = ['--x', '0', '--y', '0', '--z', '0', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'odom', '--child-frame-id', 'base_link']
+            arguments = ['--x', '1', '--y', '0', '--z', '0', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'odom', '--child-frame-id', 'base_link'],
+            parameters=[
+                {'use_sim_time': False}
+                ],
         )
     
     static_tf2_base_to_lidar = Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            arguments = ['--x', '0', '--y', '0', '--z', '0', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'lidar_link']
+            arguments = ['--x', '1', '--y', '0', '--z', '0', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'lidar_link'],
+            parameters=[
+                {'use_sim_time': False}
+                ],
         )
+
+    tf2_map_to_odom = Node(
+        package='dontscrewthepooch',
+        executable='tf2_map_to_odom'
+    )
 
     ld.add_action(static_tf2_odom_to_base)
     ld.add_action(static_tf2_base_to_lidar)
+    ld.add_action(tf2_map_to_odom)
 
     return ld
